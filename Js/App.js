3723b131d35dbb81e96056049d73953a1403937e
romalite <93836569+romalite@users.noreply.github.com>
2025-03-12 12:59:02 +0100

    Delete Js directory

--- Js/App.js
+++
@@ -1,7 +0,0 @@
-// Запрет перетаскивания для всех изображений
-document.addEventListener('DOMContentLoaded', function() {
-    const images = document.querySelectorAll('img');
-    images.forEach(img => {
-        img.setAttribute('draggable', false); // Запрет перетаскивания
-    });
-});
